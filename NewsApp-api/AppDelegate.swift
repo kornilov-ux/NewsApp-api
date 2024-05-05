5ae5351b9888fb1e18fd6eefe84f57510a01799b
Alex <alex.kornilovv2001@gmail.com>
2024-05-06 02:43:25 +0300

    removed the storyboard

---
+++ NewsApp-api/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  NewsApp-api
+//
+//  Created by Alex  on 04.05.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+					 [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		
+		let window = UIWindow()
+		window.rootViewController = ViewController()
+		window.makeKeyAndVisible()
+		
+		self.window = window
+		
+		return true
+	}
+
+
+
+
+}
+
---
+++ NewsApp-api/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp-api
+//
+//  Created by Alex  on 04.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .red
+	}
+
+
+}
+
